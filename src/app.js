50674dc6b3d94c49100118a62d8bf501f9f15190
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-01-02 21:30:40 +0700

    change wording console log ready for production

--- src/app.js
+++ src/app.js
@@ -33,5 +33,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
